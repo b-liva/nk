361ad4b7157cf70c45ca6785484026c69e615d11
name <ebammo20@gmail.com>
2020-07-31 15:02:44 +0430

    handling static files

--- nk/settings.py
+++ nk/settings.py
@@ -129,7 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
+
 AUTH_USER_MODEL = 'account.User'
 
 # defaults
