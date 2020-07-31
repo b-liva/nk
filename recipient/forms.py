07f6f2f7ac26bcbb9004ca8aa7f3f1f5c824084a
name <ebammo20@gmail.com>
2020-07-31 14:58:15 +0430

    implementing form and crud for recipient

--- recipient/forms.py
+++ recipient/forms.py
@@ -6,9 +6,27 @@ class RecipientModelForm(forms.ModelForm):
 
     class Meta:
         model = Recipient
-        fields = ('first_name', 'last_name')
+        fields = ('gender', 'first_name', 'last_name')
 
         labels = {
+            'gender': (''),
             'first_name': ('نام'),
             'last_name': ('نام خانوادگی'),
         }
+
+        widgets = {
+            'gender': forms.Select(attrs={
+                'class': 'form-control',
+
+            }),
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'نام',
+
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'نام خانودادگی',
+            }),
+        }
+
