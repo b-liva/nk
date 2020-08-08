c783194c13a263c43570cbcb6c460e85eb43139e
name <ebammo20@gmail.com>
2020-08-08 14:20:44 +0430

    report request and sent added to commitment model

--- commitment/forms.py
+++ commitment/forms.py
@@ -9,13 +9,15 @@ from commitment.models import Commitment
 class CommitmentModelForm(forms.ModelForm):
     class Meta:
         model = Commitment
-        fields = ('case', 'supporter', 'amount', 'date',)
+        fields = ('case', 'supporter', 'amount', 'date', 'report_wanted', 'report_sent')
 
         labels = {
             'case': 'پرونده',
             'supporter': 'حامی',
             'date': 'تاریخ',
             'amount': 'مبلغ',
+            'report_wanted': 'گزارش الزامی است',
+            'report_sent': 'گزارش ارسال شده است',
         }
 
     def __init__(self, *args, **kwargs):
--- commitment/models.py
+++ commitment/models.py
@@ -14,6 +14,8 @@ class Commitment(TimeStampedModel):
     case = models.ForeignKey(Case, on_delete=models.DO_NOTHING)
     amount = models.FloatField(default=0)
     date = models.DateField(default=now)
+    report_wanted = models.BooleanField(default=False)
+    report_sent = models.BooleanField(default=False)
 
     def __str__(self):
         return f"supp: {self.supporter.last_name}, recip: {self.case.recipient.first_name}, amount: {self.amount}"
