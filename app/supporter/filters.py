e81be94b688c308fcc78fe6bc6ef1de0c04f0927
name <ebammo20@gmail.com>
2020-10-09 18:32:02 +0330

    modifying supporter, filter form, details page

--- app/supporter/filters.py
+++ app/supporter/filters.py
@@ -1,10 +1,30 @@
+from django.db import models
+from django import forms
 import django_filters
+from django.forms import SelectMultiple
+
+from caseworker.models import CaseWorker
 from supporter.models import Supporter
 
 
 class SupporterFilter(django_filters.FilterSet):
-    last_name = django_filters.CharFilter(lookup_expr='icontains')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.form.fields['caseworker'].widget.attrs = {
+            'class': 'form-control'
+        }
+
+    last_name = django_filters.CharFilter(
+            lookup_expr='icontains',
+            label='حامی',
+            widget=forms.TextInput(attrs={'class': 'form-control'})
+        )
+    caseworker = django_filters.ModelChoiceFilter(
+        queryset=CaseWorker.objects.all().order_by('last_name'),
+        empty_label='انتخاب مددکار'
+    )
 
     class Meta:
         model = Supporter
-        fields = ['is_active', 'gender', 'caseworker']
+        fields = ['caseworker', 'last_name']
+
