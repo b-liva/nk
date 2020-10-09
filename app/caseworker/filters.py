98d7b2b76e62eacb035306f032d3667b0e78528f
name <ebammo20@gmail.com>
2020-10-09 18:33:30 +0330

    modifying caseworker, filter form

--- app/caseworker/filters.py
+++ app/caseworker/filters.py
@@ -1,10 +1,18 @@
+from django import forms
 import django_filters
 from caseworker.models import CaseWorker
 
 
 class CwFilter(django_filters.FilterSet):
-    last_name = django_filters.CharFilter(lookup_expr='icontains')
+    last_name = django_filters.CharFilter(
+        lookup_expr='icontains',
+        label='مددکار',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'مددکار'
+        })
+    )
 
     class Meta:
         model = CaseWorker
-        fields = ['is_active', 'gender']
+        fields = ['last_name']
